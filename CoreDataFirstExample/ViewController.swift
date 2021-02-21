231428daf404ad219c9be288273d788cfd707031
Nikhilkumar Balne <nbalne@aligntech.com>
2021-02-21 21:59:44 +0530

    Added Entity and added user to that entity.

--- CoreDataFirstExample/ViewController.swift
+++ CoreDataFirstExample/ViewController.swift
@@ -12,8 +12,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        createAnEmployee()
+        fetchAnEmployee()
     }
 
+    private func createAnEmployee(){
+        let employee = Employee(context: PersistantContainer.shared.context)
+        employee.name = "Nikhil"
+        PersistantContainer.shared.saveContext()
+    }
+    
+    private func fetchAnEmployee(){
+        
+        let path = FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask)
+        print(path[0])
+        
+        do {
+            
+            guard let employee = try PersistantContainer.shared.context.fetch(Employee.fetchRequest()) as? [Employee] else { return }
+            employee.forEach({
+                print("Name:\($0.name)")
+            })
+            
+            
+        } catch let error {
+            print("Error:\(error)")
+        }
+        
+    }
 
 }
 
